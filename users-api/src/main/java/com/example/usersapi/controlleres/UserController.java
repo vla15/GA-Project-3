e27e3b3cc325aad80605d70e4fed61fc4a74de65
Vincent La <vincela15@gmail.com>
2018-05-18 13:57:35 -0700

    setting up controllers for user api

---
+++ users-api/src/main/java/com/example/usersapi/controlleres/UserController.java
@@ -0,0 +1,50 @@
+package com.example.usersapi.controlleres;
+
+import com.example.usersapi.models.User;
+import com.example.usersapi.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/")
+    public Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUserById(@PathVariable Long userId) {
+        return userRepository.findOne(userId);
+    }
+
+    @DeleteMapping("/{userId")
+    public HttpStatus deleteUserById(@PathVariable Long userId) {
+        userRepository.delete(userId);
+        return HttpStatus.OK;
+    }
+
+    @PostMapping("/")
+    public User addNewUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUserById(@RequestBody User newUser, @PathVariable Long userId) {
+        User userFromDb = userRepository.findOne(userId);
+
+        userFromDb.setFirstName(newUser.getFirstName());
+        userFromDb.setLastName(newUser.getLastName());
+        userFromDb.setAge(newUser.getAge());
+        userFromDb.setEthnicity(newUser.getEthnicity());
+        userFromDb.setOccupation(newUser.getOccupation());
+        userFromDb.setInterests(newUser.getInterests());
+        userFromDb.setProfile(newUser.getProfile());
+
+        return userRepository.save(userFromDb);
+    }
+}
---
+++ users-api/src/main/java/com/example/usersapi/models/User.java
@@ -0,0 +1,40 @@
+package com.example.usersapi.models;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "USERS")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    @Column(name = "AGE")
+    private int age;
+
+    @Column(name = "OCCUPATION")
+    private String occupation;
+
+    @Column(name = "ETHNICITY")
+    private String ethnicity;
+
+    @Column(name = "INTERESTS")
+    private String interests;
+
+    @Column(name = "PROFILE")
+    private String profile;
+}
