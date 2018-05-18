511e934e3777afc080625ef021ee3d8c66bd9357
Vincent La <vincela15@gmail.com>
2018-05-18 14:05:14 -0700

    adding hard coded data to postgresql database flyway migrations

---
+++ flyway/sql/V1__Create_user_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE USERS (
+  ID serial,
+  FIRST_NAME varchar(100),
+  LAST_NAME varchar(100),
+  AGE integer,
+  ETHNICITY varchar(200),
+  INTERESTS varchar(500),
+  PROFILE varchar(200),
+  OCCUPATION varchar(200)
+);
---
+++ flyway/sql/afterMigrate.sql
@@ -0,0 +1,6 @@
+INSERT INTO USERS 
+(FIRST_NAME, LAST_NAME, AGE, ETHNICITY, INTERESTS, PROFILE, OCCUPATION)
+VALUES
+('Mary', 'Liquor', 25, 'Non conforming human', 'Sports, Sports, Sports', 'assets/generic_women_1.jpeg', 'Nurse'),
+('Jac', 'Star', 32, 'Alooof', 'Weapons', 'assets/generic_women_1.jpeg', 'Lawyer'),
+('Wayne', 'Carson', 21, 'Bacon', 'Loves to cook', 'assets/generic_women_1.jpeg', 'Chef');
