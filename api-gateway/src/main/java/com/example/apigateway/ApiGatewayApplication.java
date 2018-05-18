ea1b542e4752070dff7888be746cfbda95f5f64b
Vincent La <vincela15@gmail.com>
2018-05-18 13:35:03 -0700

    setting up zuul api gateway

--- api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,13 @@ package com.example.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableZuulProxy
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 }
