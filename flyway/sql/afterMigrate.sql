ef35c4827def36ee96ab815cb55e20069beb1639
Vincent La <vincela15@gmail.com>
2018-05-21 22:29:40 -0700

    updating flyway files and styling

--- flyway/sql/afterMigrate.sql
+++ flyway/sql/afterMigrate.sql
@@ -1,6 +1,6 @@
 INSERT INTO USERS 
-(FIRST_NAME, LAST_NAME, AGE, ETHNICITY, INTERESTS, PROFILE, OCCUPATION)
+(FIRST_NAME, LAST_NAME, AGE, ETHNICITY, INTERESTS, OCCUPATION)
 VALUES
-('Mary', 'Liquor', 25, 'Non conforming human', 'Sports, Sports, Sports', 'assets/generic_women_1.jpeg', 'Nurse'),
-('Jac', 'Star', 32, 'Alooof', 'Weapons', 'assets/generic_women_1.jpeg', 'Lawyer'),
-('Wayne', 'Carson', 21, 'Bacon', 'Loves to cook', 'assets/generic_women_1.jpeg', 'Chef');
+('Mary', 'Liquor', 25, 'Non conforming human', 'Sports, Sports, Sports', 'Nurse'),
+('Jac', 'Star', 32, 'Alooof', 'Weapons', 'Lawyer'),
+('Wayne', 'Carson', 21, 'Bacon', 'Loves to cook', 'Chef');
