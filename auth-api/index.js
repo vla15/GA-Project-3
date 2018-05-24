e3536a813f23140f2c5e918f725c48b3883d3ccb
Vincent La <vincela15@gmail.com>
2018-05-24 16:19:07 -0700

    adding eureka server connection to node auth server

--- auth-api/index.js
+++ auth-api/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const app = express();
+const express = require('express')
 
+const app = express();
 app.get('/', (req, res) => {
   res.send('hello we are up and running');
 })
@@ -12,3 +12,34 @@ app.listen(8084, err => {
     console.log('Listening in on port number 8084');
   }
 })
+
+const Eureka = require('eureka-js-client').Eureka;
+
+const client = new Eureka({
+  instance: {
+    app: 'authservice',
+    hostName: 'localhost',
+    ipAddr: '127.0.0.1',
+    port: {
+      '$': 8084,
+      '@enabled': true,
+    },
+    statusPageUrl: 'http://localhost:8084',
+    vipAddress: 'jq.test.something.com',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    },
+  },
+  eureka: {
+    host: 'localhost',
+    port: 8761,
+    servicePath: '/eureka/apps'
+  },
+});
+
+client.logger.level('debug');
+
+client.start((error) => {
+  console.log(error || 'complete');
+});
