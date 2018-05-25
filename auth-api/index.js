d29ad932371bc05176906d1dd7a74a06477947a8
Vincent La <vincela15@gmail.com>
2018-05-25 11:59:45 -0700

    adding ndoe js to docker compose file.  Removing private variables from angular components which prevented ng build from working

--- auth-api/index.js
+++ auth-api/index.js
@@ -1,45 +1,47 @@
 const express = require('express')
 
+const Eureka = require('eureka-js-client').Eureka;
+const client = new Eureka({
+  instance: {
+    app: 'auth',
+    hostName: 'auth',
+    ipAddr: 'auth',
+    statusPageUrl: 'http://auth:3000',
+    port: {
+      '$': 3000,
+      '@enabled': 'true',
+    },
+    vipAddress: 'auth',
+    dataCenterInfo: {
+      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
+      name: 'MyOwn',
+    }
+  },
+  eureka: {
+    host: 'eureka',
+    port: 8761,
+    servicePath: '/eureka/apps/'
+  }
+});
+
+client.logger.level('debug');
+
+setTimeout(function () {
+  client.start(function (error) {
+    console.log(error || 'complete');
+  });
+}, 180000);
+
 const app = express();
 app.get('/', (req, res) => {
-  res.send('hello we are up and running');
+  console.log('firing off');
+  res.status(200).send('hello we are up and running');
 })
 
-app.listen(8084, err => {
+app.listen(3000, err => {
   if (err) {
     console.log('errror', err)
   } else {
     console.log('Listening in on port number 8084');
   }
 })
-
-const Eureka = require('eureka-js-client').Eureka;
-
-const client = new Eureka({
-  instance: {
-    app: 'authservice',
-    hostName: 'localhost',
-    ipAddr: '127.0.0.1',
-    port: {
-      '$': 8084,
-      '@enabled': true,
-    },
-    statusPageUrl: 'http://localhost:8084',
-    vipAddress: 'jq.test.something.com',
-    dataCenterInfo: {
-      '@class': 'com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo',
-      name: 'MyOwn',
-    },
-  },
-  eureka: {
-    host: 'localhost',
-    port: 8761,
-    servicePath: '/eureka/apps'
-  },
-});
-
-client.logger.level('debug');
-
-client.start((error) => {
-  console.log(error || 'complete');
-});
